9124ab25f2527afac6e1a115a48a997c5c5b6185
liufan421 <liufan421@gmail.com>
2020-10-24 16:28:25 +0800

    set default font size after init

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -10,6 +10,13 @@
   (setq sml/no-confirm-load-theme t
 	sml/theme 'respectful)
   (sml/setup))
+;; 直接执行 set-face-attribute 和 set-frame-font 会不生效，在完成初始化后再执行才生效
+;; set-face-attribute 可以效果比 set-frame-font 好一些，交互的 mini-buffer 也能生效
+;; set-default-font 是 set-frame-font 的别名，emacs 27 之后就删除了，所以目前版本中使用 set-default-font 会报错
+(add-hook 'after-init-hook
+	  #'(lambda()
+	      (set-face-attribute 'default nil :font "-*-Source Code Pro-normal-normal-normal-*-18-*-*-*-m-0-iso10646-1")))
+	      ;; (set-frame-font "-*-Source Code Pro-normal-normal-normal-*-18-*-*-*-m-0-iso10646-1" t)))
 
 (provide 'init-ui)
 ;;; init-ui.el ends here
