ad46957b3357d01c841c9e0b51e5e4a98ea64c77
liufan421 <liufan421@gmail.com>
2020-10-25 01:19:07 +0800

    add syntax highlighting to src in org file
    Signed-off-by: liufan421 <liufan421@gmail.com>

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -1,7 +1,12 @@
 ;;; package --- emacs org 相关配置
 ;;; Commentary:
 ;;; Code:
+
+;; 自动换行
 (add-hook 'org-mode-hook (lambda () (setq truncate-lines nil)))
+;; 文本内语法高亮
+(require 'org)
+(setq org-src-fontify-natively t)
 
 (provide 'init-org)
 ;;; init-org.el ends here
