b0b26ccfbc56de215d1c34def0ceb04722e93093
liufan421 <liufan421@gmail.com>
2020-10-25 01:55:23 +0800

    disable auto save
    Signed-off-by: liufan421 <liufan421@gmail.com>

--- custom.el
+++ custom.el
@@ -6,6 +6,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(auto-save-default nil)
  '(display-line-numbers-type 'relative)
  '(global-display-line-numbers-mode t)
  '(package-selected-packages
